4fedcc03805e62ba3280f2d1ddc044ce7fc7067a
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-09-05 17:09:05 +0700

    commit 01

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,35 @@ fun main(args: Array<String>) {
     val myRunnable = MyRunnable()
     val myThread = Thread(myRunnable, "New_Thread")
     myThread.start()
+    sleepThread(6)
+    myRunnable.stopThread()
     println("The end ${getThreadName()}")
 }
 
 class MyRunnable : Runnable {
+    private var check = false
+
+    fun stopThread() {
+        check = true
+    }
+
+    private fun stillRunning() = !check
+
     override fun run() {
-        println("Runnable run ${getThreadName()}")
+        var i = 0
+        while (stillRunning()) {
+            println("Runnable run step $i ${getThreadName()}")
+            sleepThread(2)
+            i += 1
+        }
     }
 }
 
-private fun getThreadName() : String {
+private fun getThreadName(): String {
     return "at thread name ${Thread.currentThread().name}"
 }
 
-private fun sleepThread(second : Long) {
+private fun sleepThread(second: Long) {
     try {
         Thread.sleep(second * 1000L)
     } catch (e: InterruptedException) {
