f6aea871000ac6f4988638ca80f68f35567cfe7e
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-09-05 14:24:28 +0700

    commit 01

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello World! ${getThreadName()}")
+    val myThread = MyThread()
+    myThread.start()
+    sleepThread(5)
+    println("The end ${getThreadName()}")
+}
+
+private fun getThreadName() : String {
+    return "at thread name ${Thread.currentThread().name}"
+}
+
+class MyThread : Thread("New_Thread") {
+    override fun run() {
+        sleepThread(2)
+        println("Running ${getThreadName()}")
+    }
+}
+
+private fun sleepThread(second : Long) {
+    try {
+        Thread.sleep(second * 1000L)
+    } catch (e: InterruptedException) {
+        e.printStackTrace()
+    }
 }
