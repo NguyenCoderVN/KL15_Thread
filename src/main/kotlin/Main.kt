d45d49b0b2c448c8ca0e6d51b56d32a7d92eeb8d
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-09-11 19:06:40 +0700

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,13 @@
 fun main() {
-    val a = 5
-    val b = 5
+    val a = 2
+    val b = 4
     val result: Int = sum(a, b)
-    println("Result of (a + b)*5 is $result")
+    println("Result of (2 + 4)*5 is $result")
 }
 
 fun sum(a: Int, b: Int): Int {
     val c = a + b
-    val d = multi(c, 5)
-    return c
+    return multi(c, 5)
 }
 
 fun multi(x: Int, y: Int): Int {
