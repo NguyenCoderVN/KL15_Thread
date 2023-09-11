e30e8e3dd2d61cd387b7b627808be3c6bc6809c2
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-09-11 22:08:46 +0700

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,23 @@
 fun main() {
-    val a = 5
-    val b = 5
-    val result: Int = sum(a, b)
-    println("Result of (5 + 5) * 5 is $result")
+
+    val a: Byte = 112
+    val b: Short = 11245
+    val c: Int =  100000
+    val d: Long = 9999L
+    val e: Double = 999.5
+    val f: Float = 19.5F
+    val letter: Char = 'a'
+    val flag: Boolean = true
+    val array: Array<String> = arrayOf("1","2","3")
+    val shape = Shape()
+    shape.show()
 }
 
-fun sum(a: Int, b: Int): Int {
-    val c = a + b
-    return multi(c, 5)
-}
+class Shape {
+     var aShape : Byte = 15
 
-fun multi(x: Int, y: Int): Int {
-    return x * y
+    fun show() {
+        val i : Int = 10
+        println("This is shape")
+    }
 }
